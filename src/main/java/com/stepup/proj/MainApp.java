dfa6be1e3296bb128d327383e678056a85d2ceca
Молоков Александр Викторович <molokovav@dev.vtb.ru>
2023-12-21 14:27:01 +0700

    поменяли строки

--- src/main/java/com/stepup/proj/MainApp.java
+++ src/main/java/com/stepup/proj/MainApp.java
@@ -3,7 +3,7 @@ package com.stepup.proj;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("11111");
         System.out.println("222222");
+        System.out.println("3");
     }
 }
